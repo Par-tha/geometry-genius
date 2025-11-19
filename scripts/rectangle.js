d781c879fdf372c4b91a6dc0ea9281cbd1aa624c
Par-tha <parthamondal16.pm@gmail.com>
2025-11-19 18:01:02 +0600

    rectangle added

---
+++ scripts/rectangle.js
@@ -0,0 +1,15 @@
+function calculateRectangleArea (){
+    const rectangleWidthArea = document.getElementById('rectangleWidthInput');
+    const rectangleWidth = rectangleWidthArea.value;
+    const width = parseFloat(rectangleWidth);
+    console.log(width);
+    
+    const rectangleLengthArea = document.getElementById('rectangleLengthInput');
+    const rectangleLength = rectangleLengthArea.value;
+    const length = parseFloat(rectangleLength);
+    console.log(length);
+
+    const area = width*length;
+    const rectangleArea = document.getElementById('rectangle-Area');
+    rectangleArea.innerText = area;
+}
